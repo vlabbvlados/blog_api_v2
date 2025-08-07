c9064233059281602fd8c050f9b8d2cfa58079f2
vlabbvlados <159616731+vlabbvlados@users.noreply.github.com>
2025-08-07 17:08:53 +0200

    fix3

--- src/main/java/com/example/blog_api_v2/JpaUserDetailsService.java
+++ src/main/java/com/example/blog_api_v2/JpaUserDetailsService.java
@@ -3,7 +3,9 @@ package com.example.blog_api_v2;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JpaUserDetailsService implements UserDetailsService{
 	private final UserRepository userRepository;
 	
--- src/main/java/com/example/blog_api_v2/SecurityConfig.java
+++ src/main/java/com/example/blog_api_v2/SecurityConfig.java
@@ -2,15 +2,38 @@ package com.example.blog_api_v2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration @EnableWebSecurity
 public class SecurityConfig {
+	private final JpaUserDetailsService jpaUserDetailsService;
 	
+	public SecurityConfig(JpaUserDetailsService jpaUserDetailsService) {
+		super();
+		this.jpaUserDetailsService = jpaUserDetailsService;
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public SecurityFilterChain sucurityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults());
+		
+		
+		return http.build();
+	}
+	
 }
