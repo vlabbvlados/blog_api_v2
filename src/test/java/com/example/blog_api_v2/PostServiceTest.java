663078d6448f1bf407f3c45666adb2f1782f1164
vlabbvlados <159616731+vlabbvlados@users.noreply.github.com>
2025-09-18 16:47:05 +0200

    Create PostServiceTest.java

---
+++ src/test/java/com/example/blog_api_v2/PostServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.blog_api_v2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+	@Mock
+	private PostRepository postRepository;
+	
+	@InjectMocks
+	private PostService postService;
+	
+	@Spy
+	Post expectedPost;
+	
+	@Test
+	public void testFindByPostId() {
+		when(postRepository.findById(1L)).thenReturn(Optional.of(expectedPost));
+		Post actualPost = postService.getPostFromId(1L);
+		assertEquals(expectedPost, actualPost);
+	}
+}
