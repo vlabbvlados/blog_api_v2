6c8b2a73dff9ccea970e1f87daf7b3078e68ae51
vlabbvlados <159616731+vlabbvlados@users.noreply.github.com>
2025-10-05 07:43:35 +0200

    Update PostServiceTest.java

--- src/test/java/com/example/blog_api_v2/PostServiceTest.java
+++ src/test/java/com/example/blog_api_v2/PostServiceTest.java
@@ -3,6 +3,7 @@ package com.example.blog_api_v2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -20,13 +21,20 @@ public class PostServiceTest {
 	@InjectMocks
 	private PostService postService;
 	
-	@Spy
-	Post entityPost;
+	private Post entityPost;
+	private PostResponse expectedResponse;
+	
 	
 	@Test
 	public void getPostFromId() {
 		entityPost.setContent("ulala");
 		entityPost.setTitle("NAME");
+		entityPost.setId(1L);
+		entityPost.setLikes(33);
+		LocalDateTime fixedDate = LocalDateTime.of(2025, 10, 5, 7, 0, 0);
+		entityPost.setCreationDate(fixedDate);
+		expectedResponse.setContent("ulala");
+		expectedResponse.setTitle("NAME");
 		when(postRepository.findById(1L)).thenReturn(Optional.of(entityPost));
 		PostResponse actualResponse = postService.getPostFromId(1L);
 		assertEquals(expectedResponse, actualResponse);
