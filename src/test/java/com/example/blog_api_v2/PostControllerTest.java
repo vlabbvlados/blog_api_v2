8ece1abfd44213d042699981c332ceaf36fb156e
vlabbvlados <159616731+vlabbvlados@users.noreply.github.com>
2025-11-09 08:34:49 +0100

    Update PostControllerTest.java

--- src/test/java/com/example/blog_api_v2/PostControllerTest.java
+++ src/test/java/com/example/blog_api_v2/PostControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,9 +20,9 @@ public class PostControllerTest {
 	private ObjectMapper objectMapper;
 	
 	@Test
-	public void testGetPostById() {
+	public void testGetPostById_Success() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
-		.get("/posts/2"))
+		.get("/posts/{postId}", 2))
 		.andExpect(status().isOk())
 		.andExpect(MockMvcResultMatchers.jsonPath("$.title").exists())
 		.andExpect(MockMvcResultMatchers.jsonPath("$.content").exists());
