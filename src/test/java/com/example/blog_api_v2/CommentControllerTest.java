b5698fec8a2eb6e12657cd832ddeed3ee4207c70
vlabbvlados <159616731+vlabbvlados@users.noreply.github.com>
2025-11-25 23:48:42 +0100

    Create CommentControllerTest.java

---
+++ src/test/java/com/example/blog_api_v2/CommentControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.blog_api_v2;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CommentControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Test
+	public void testGetCommentFromPost_success() throws Exception{
+		
+	}
+	
+}
